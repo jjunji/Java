c32a92c1e17431d3ca20d8408819a73746d79f80
jjunji <jh.jun92@gmail.com>
2018-01-08 19:10:18 +0900

    sub2 complete

---
+++ ch7/SuperTest2/src/SuperTest2.java
@@ -0,0 +1,20 @@
+public class SuperTest2 {
+	public static void main(String[] args) {
+		Child c = new Child();
+		c.method();	
+	}
+}
+
+class Parent{
+	int x = 10;
+}
+
+class Child extends Parent{
+	int x = 20;
+	
+	void method(){
+		System.out.println("x= " + x);  // 20
+		System.out.println("this.x = " + this.x);  // 20
+		System.out.println("super.x = " + super.x);  // 10
+	}
+}
--- ch7/TVCR/src/TVCR.java
+++ ch7/TVCR/src/TVCR.java
@@ -1,4 +1,36 @@
+class Tv{
+	boolean power;
+	int channel;
+	
+	void power(){ power = !power; }
+	void channelUp(){ ++channel;}
+	void channelDown(){--channel;}
+	
+}
 
-public class TVCR {
+class VCR{
+	boolean power;
+	int counter = 0;
 
+	void power(){ power = !power; }
+	void play(){}
+	void stop(){}
+	void rew(){}
+}
+
+class TVCR extends Tv{
+	VCR vcr = new VCR();
+	int counter = vcr.counter;
+	
+	void play(){
+		vcr.play();
+	}
+	
+	void stop(){
+		vcr.stop();
+	}
+	
+	void rew(){
+		vcr.rew();
+	}
 }
