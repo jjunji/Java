0aeb1c3c0ccd90686d31ec6c1471e70ca63e4a2f
jjunji <jh.jun92@gmail.com>
2018-01-09 18:25:00 +0900

    sub4 complete

---
+++ ch7/FinalCardTest/src/FianlCardTest.java
@@ -0,0 +1,27 @@
+class FianlCardTest {
+
+	public static void main(String[] args) {
+		Card c = new Card("Heart", 10);
+		//c.NUMBER = 5;
+		System.out.println(c.KIND);
+		System.out.println(c.NUMBER);
+	}
+
+}
+
+class Card{
+	final int NUMBER;
+	final String KIND;
+	
+	static int width = 100;
+	static int height = 250;
+	
+	Card(String kind, int num){
+		KIND = kind;
+		NUMBER = num;
+	}
+	
+	Card(){
+		this("HEART", 1);
+	}
+}
