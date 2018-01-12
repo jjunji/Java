0197740feb9d85950d675156888263e08e6cd8f9
jjunji <jh.jun92@gmail.com>
2018-01-13 00:08:06 +0900

    sub6 complete

---
+++ ch7/ParserTest/src/ParserTest.java
@@ -0,0 +1,43 @@
+interface Parseable{
+	// 구문 분석작업을 수행한다.
+	public abstract void parse(String fileName);
+}
+
+class ParserManager{
+	// 리턴타입이 Parseable 인터페이스이다.
+	public static Parseable getParser(String type){
+		if(type.equals("XML")){
+			return new XMLParser();
+		}else{
+			Parseable p = new HTMLParser();
+			return p;
+			// return new HTMLParser();
+		}
+	}
+}
+
+class XMLParser implements Parseable{
+
+	@Override  // 오버라이딩 단축키 alt + shift + s
+	public void parse(String fileName) {
+		System.out.println(fileName + "-xml parsing completed.");
+	}
+}
+
+class HTMLParser implements Parseable{
+
+	@Override
+	public void parse(String fileName) {
+		System.out.println(fileName + "-html parsing completed.");
+	}
+	
+}
+
+public class ParserTest {
+	public static void main(String[] args) {
+		Parseable parser = ParserManager.getParser("XML");
+		parser.parse("document.xml");
+		parser = ParserManager.getParser("HTML");
+		parser.parse("document2.html");
+	}
+}
---
+++ ch7/RepairableTest/src/RepairableTest.java
@@ -0,0 +1,85 @@
+public class RepairableTest {
+	public static void main(String[] args) {
+		Tank tank = new Tank();
+		DropShip dropShip = new DropShip();
+		
+		Marine marine = new Marine();
+		
+		SCV scv = new SCV();
+		
+		scv.repair(tank);
+		scv.repair(dropShip);
+		//scv.repair(marine); -> 에러
+	}
+}
+
+interface Repairable{}
+
+class Unit{
+	int hitPoint;
+	final int MAX_HP;
+	
+	Unit(int hp){
+		MAX_HP = hp;
+	}
+}
+
+class GroundUnit extends Unit{
+	GroundUnit(int hp){
+		super(hp);
+	}
+}
+
+class AirUnit extends Unit{
+	AirUnit(int hp) {
+		super(hp);
+	}
+}
+
+class Tank extends GroundUnit implements Repairable{
+	Tank(){
+		super(150);
+		hitPoint = MAX_HP;
+	}
+	
+	public String toString(){
+		return "Tank";
+	}
+}
+
+class DropShip extends AirUnit implements Repairable{
+
+	public DropShip() {
+		super(125);
+		hitPoint = MAX_HP;
+	}
+	
+	public String toString(){
+		return "Dropship";
+	}
+	
+}
+
+class Marine extends GroundUnit{
+	Marine(){
+		super(40);
+		hitPoint = MAX_HP;
+	}
+}
+
+class SCV extends GroundUnit implements Repairable{
+	SCV(){
+		super(60);
+		hitPoint = MAX_HP;
+	}
+	
+	void repair(Repairable r){
+		if(r instanceof Unit){
+			Unit u = (Unit) r;
+			while(u.hitPoint != u.MAX_HP){
+				u.hitPoint ++;
+			}
+			System.out.println(u.toString() + "수리가 끝났습니다.");
+		}
+	}
+}
