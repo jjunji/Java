fb8c17f5ad236fa84301f5caeff48fed03afce77
jjunji <jh.jun92@gmail.com>
2018-01-25 19:17:10 +0900

    sub1.5 complete

---
+++ ch11/IteratorEx2/src/IteratorEx2.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class IteratorEx2 {
+	public static void main(String[] args) {
+		ArrayList original = new ArrayList(10);
+		ArrayList copy1 = new ArrayList(10);
+		ArrayList copy2 = new ArrayList(10);
+		
+		for(int i=0; i<10; i++){
+			original.add(i);
+		}
+		Iterator it = original.iterator();
+		
+		while(it.hasNext()){
+			copy1.add(it.next());
+		}
+		
+		System.out.println("= From Original to copy1 copy = ");
+		System.out.println("Original : " + original);
+		System.out.println("copy1 : " + copy1);
+		System.out.println();
+		
+		it = original.iterator(); // Iterator은 재사용이 안되므로, 다시 얻어와야 한다.
+		
+		while(it.hasNext()){
+			copy2.add(it.next());
+			it.remove();
+		}
+		
+		System.out.println("= Origianl에서 copy2로 이동 =");
+		System.out.println("original : " + original);
+		System.out.println("copy2 : " + copy2);
+	}
+}
